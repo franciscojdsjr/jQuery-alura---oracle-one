7ccf51f002956989b7b3200816ae02a294166895
Francisco Jose da Silva Junior <franciscojdsjr@gmail.com>
2021-02-13 17:42:05 -0300

    aula5

--- public/js/main.js
+++ public/js/main.js
@@ -80,29 +80,24 @@ function inicializaBordaValidadora () {
 
 
 
-function inserePlacar(){
-
+function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Francisco";
-    var numeroPalavras = $("#contador-palavras").text();
+    var usuario = "Francisco Júnior"
+    var numPalavras = $("#contador-palavras").text();
 
-    var linha = novalinha(usuario,numeroPalavras);
+    var linha = novaLinha(usuario, numPalavras);
     linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.append(linha);
+}
 
-
-
-};
-
-
-function novalinha(usuario,palavras){
-
+function novaLinha(usuario, palavras) {
     var linha = $("<tr>");
     var colunaUsuario = $("<td>").text(usuario);
     var colunaPalavras = $("<td>").text(palavras);
     var colunaRemover = $("<td>");
-    var link = $("<a>").addClass("botao-remover").attr("href","#");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
     var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
     link.append(icone);
@@ -114,19 +109,12 @@ function novalinha(usuario,palavras){
     linha.append(colunaRemover);
 
     return linha;
+}
 
-
-};
-
-
-function removeLinha() {
-$(".botao-remover").click(function(event){
-
+function removeLinha(event) {
     event.preventDefault();
     $(this).parent().parent().remove();
-
-});};
-
+}
 
 function reiniciaJogo() {
 
@@ -148,4 +136,5 @@ function finalizaJogo() {
     campo.attr("disabled", true);
     campo.addClass("campo-desativado");
     inserePlacar();
+    removeLinha();
 };
