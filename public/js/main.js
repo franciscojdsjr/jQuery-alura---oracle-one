181ffd412da0f1a6fe4aaf8174ff656f0a32c435
Francisco Jose da Silva Junior <franciscojdsjr@gmail.com>
2021-02-13 17:33:24 -0300

    aula5

--- public/js/main.js
+++ public/js/main.js
@@ -86,7 +86,46 @@ function inserePlacar(){
     var usuario = "Francisco";
     var numeroPalavras = $("#contador-palavras").text();
 
+    var linha = novalinha(usuario,numeroPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+
+
+
 };
+
+
+function novalinha(usuario,palavras){
+
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+    var link = $("<a>").addClass("botao-remover").attr("href","#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+
+
+};
+
+
+function removeLinha() {
+$(".botao-remover").click(function(event){
+
+    event.preventDefault();
+    $(this).parent().parent().remove();
+
+});};
 
 
 function reiniciaJogo() {
